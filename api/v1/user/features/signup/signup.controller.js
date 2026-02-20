24b6a50941517abea33f2b6dac9cc62b6fa4eaac
jd1v <hjd1831da@gmail.com>
2026-02-20 15:16:59 +0330

    Create dto in signup controller

--- api/v1/user/features/signup/signup.controller.js
+++ api/v1/user/features/signup/signup.controller.js
@@ -1,5 +1,23 @@
-const newUser = async (req, res) => {
+const dto = require('./signup.dto');
+const logger = require('@utils/logger');
+const {ZodError: ZodError} = require('zod');
 
+const newUser = async (req, res) => {
+    try {
+        const cleanBody = dto.sanitizeSignupDTO.parse(req.body);
+    } catch (err) {
+        if (err instanceof ZodError) {
+            logger.warn("Signup validation failed", {
+                path: req.originalUrl,
+                ip: req.ip,
+                error: err.issues.map(issue => issue.message)
+            });
+            return res.status(400).json({
+                message: "Validation failed",
+                errors: err.issues.map(issue => issue.message)
+            });
+        }
+    }
 }
 
 module.exports = {
