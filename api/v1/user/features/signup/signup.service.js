cce91f3edb16ecdfe8d7cb176ac619b3a8a12514
jd1v <hjd1831da@gmail.com>
2026-02-20 17:19:43 +0330

    Create hashing password in signup service

--- api/v1/user/features/signup/signup.service.js
+++ api/v1/user/features/signup/signup.service.js
@@ -1,6 +1,19 @@
-const newUser = async (reqBody) => {
+const argon2 = require('argon2');
 
+const newUser = async (reqBody) => {
+    delete reqBody.confirmPassword;
+    const passwordHashed = await hashPassword(reqBody.password);
 };
+
+
+async function hashPassword(password) {
+    return await argon2.hash(password, {
+        type: argon2.argon2id,
+        memoryCost: 2 ** 16,
+        timeCost: 3,
+        parallelism: 1
+    });
+}
 
 module.exports = {
     newUser: newUser,
