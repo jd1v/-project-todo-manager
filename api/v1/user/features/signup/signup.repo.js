74d5d0802ad8e677eb39cdcdb88b405436f724a5
jd1v <hjd1831da@gmail.com>
2026-02-21 22:56:28 +0330

    Add checking unique function in repo signup

--- api/v1/user/features/signup/signup.repo.js
+++ api/v1/user/features/signup/signup.repo.js
@@ -1,7 +1,20 @@
 const model = require('@v1user/user.model');
 
 const createUser = async (data) => {
+    // noinspection JSCheckFunctionSignatures
     return await model.create(data);
+}
+
+const existPhone = async (phone) => {
+    return !!(await model.exists({phone}));
+}
+
+const existEmail = async (email) => {
+    return !!(await model.exists({email}));
+}
+
+const nationalCode = async (nationalCode) => {
+    return !!(await model.exists({nationalCode}));
 }
 
 module.exports = {
